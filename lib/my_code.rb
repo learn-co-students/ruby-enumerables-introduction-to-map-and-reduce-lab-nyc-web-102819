086575f3e0bd370bd8cd516228cbd219da58de12
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-10 21:35:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,27 +1,72 @@
 source_array = [1, 2, 3, -9]
 
 def map_to_negativize(source_array)
-  source_array.map {|n| n*-1}
+  array = []
+  counter = 0 
+  while counter < source_array.size do
+    array << source_array[counter] * -1
+    counter += 1 
+  end
+  array
 end
 
 def map_to_no_change(dune)
-  dune.map {|n| n}
+  array = []
+  counter = 0 
+  while counter < dune.size do
+    array << dune[counter]
+    counter += 1 
+  end
+  array
 end
 
+
 def map_to_double(source_array)
-  source_array.map {|n| n*2}
+  array = []
+  counter = 0 
+  while counter < source_array.size do
+    array << source_array[counter] * 2 
+    counter += 1 
+  end
+  array 
 end
 
 def map_to_square(source_array)
-  source_array.map {|n| n**2}
+  array = []
+  counter = 0 
+  while counter < source_array.size do
+    array << source_array[counter] ** 2 
+    counter += 1 
+  end
+  array
 end
 
-def reduce_to_total(source_array, starting_point)
-  source_array.reduce(0)
+def reduce_to_total(source_array, starting_point=0)
+  total = starting_point
+  counter = 0 
+  while counter < source_array.size do
+    total += source_array[counter] 
+    counter += 1 
+  end
+  total 
 end
 
 def reduce_to_all_true(source_array)
+  counter = 0 
+  while counter < source_array.size do
+    return false if 
+    source_array[counter] == false 
+    counter += 1 
+  end
+  return true 
 end
 
 def reduce_to_any_true(source_array)
+  counter = 0 
+  while counter < source_array.size do
+    return true if 
+    source_array[counter] == true 
+    counter += 1 
+  end
+  return false 
 end
