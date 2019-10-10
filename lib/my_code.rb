3a301d5a70bc57a0fec7ec02d2bdcc489e733a82
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-10 20:49:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# My Code here....
+source_array = [1, 2, 3, -9]
+
+def map_to_negativize(source_array)
+  source_array.map {|n| n*-1}
+end
+
+def map_to_no_change(dune)
+  dune.map {|n| n}
+end
+
+def map_to_double(source_array)
+  source_array.map {|n| n*2}
+end
+
+def map_to_square(source_array)
+  source_array.map {|n| n**2}
+end
+
+def reduce_to_total(source_array, starting_point)
+  source_array.reduce(0)
+end
+
+def reduce_to_all_true(source_array)
+end
+
+def reduce_to_any_true(source_array)
+end
