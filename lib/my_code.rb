8ce2bc6c8cd6449983170c45f8e7cf0361a21a91
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-26 17:39:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,80 +1,77 @@
 # My Code here....
 
-def map_to_negativize
-  count = 0
-  negativize = []
-  
-  while count < source_array.count do
-    negativize << source_array[count] * (-1)
-    count += 1
-  end
-  
-  return negativize
+def map_to_negativize(source_array)
+count = 0
+negative = -1
+while count < source_array.length do
+source_array[count] = source_array[count] * negative
+source_array[count]
+count += 1
+end
+source_array
 end 
 
 
 def map_to_no_change(source_array)
-  no_change = source_array
-  
-  return no_change
+ array = source_array
+
 end
 
 
 def map_to_double(source_array)
-  count = 0
-  double = []
-  
-  while count < source_array.count do
-    double << source_array[count] * (2)
-    count += 1
+count = 0
+double = 2
+while count < source_array.length do
+source_array[count] = source_array[count] * double
+source_array[count]
+count += 1
+end
+source_array
 
 end
 
 def map_to_square(source_array)
-  count = 0
-  square = []
-  
-  while count < source_array.count do
-    square << source_array[count] ** (2)
-    count += 1
-  end
-  
-  return square
+count = 0
+square = 2
+  while count < source_array.length do
+  source_array[count] = source_array[count]**square
+  source_array[count]
+  count += 1
+end 
+source_array
 end 
 
 
 def reduce_to_total(source_array, starting_point=0)
-  total = 0
+  index = 0 
+  output = starting_point
   
-  if starting_point > 0
-    count = starting_point
-    while count < source_array.count do
-      total += source_array[count]
-      count += 1
-      return total
-    end
-  else
-    while starting_point < source_array.count do
-      total += source_array[starting_point]
-      starting_point += 1
-      return total
-    end
-
+  while index < source_array.length do
+      output += source_array[index]
+      index += 1
+end
+output
 end
 
 
 def reduce_to_all_true(source_array)
- if source_array.include?false
-    return false
-  else
-    return true
-  en
+  i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == false)
+     return false
+  end
+      i += 1
+  end
+   true
 end 
 
 def reduce_to_any_true(source_array)
-  if source_array.include?true
-    return true
-  else
-    return false
+ i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == true)
+     return true
   end
+      i += 1
+  end
+   false
 end
